aa90cbbeed19ba1b04900d1dbb2eba944ba29ce8
Jared <jbates9210@gmail.com>
2018-04-06 19:57:58 -0400

    Built initial storyboard and added placeholder code to test table

--- ToDoApp/FirstViewController.swift
+++ ToDoApp/FirstViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
